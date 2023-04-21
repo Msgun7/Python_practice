099df55763f39b21bd5377bde112a64457712b32
Msgun7 <fgarr@naver.com>
2023-04-21 14:11:40 +0900

    과제 추가제출

--- 4_18_BaekPrac2.py
+++ 4_18_BaekPrac2.py
@@ -4,7 +4,7 @@ T = int(input())
 
 for i in range(T):
     a, b = map(int, input().split())
-    a + b
-    print(a+b)
+    sum = a + b
+    print(sum)
 
 # T를 입력받고 반복문 안에 T만큼의 횟수를 입력받도록 수식을 넣어 합을 그때그때 출력하는방식으로 짜보았습니다.
---
+++ 4_21_BaekPrac.py
@@ -0,0 +1,12 @@
+TestPoint = int(input())
+
+if 90 <= TestPoint <= 100:
+    print("A")
+elif 80 <= TestPoint <= 89:
+    print("B")
+elif 70 <= TestPoint <= 79:
+    print("C")
+elif 60 <= TestPoint <= 69:
+    print("D")
+else:
+    print("F")
